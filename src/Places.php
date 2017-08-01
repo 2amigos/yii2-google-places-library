7e3395b5dab16b80efa0b5ebaabb8f406d14ec63
Antonio Ramirez <amigo.cobos@gmail.com>
2017-08-01 17:46:58 +0200

    update licenses

--- src/Places.php
+++ src/Places.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the 2amigos/yii2-google-places-library project.
+ *
+ * (c) 2amigOS! <http://2amigos.us/>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
 namespace dosamigos\google\places;
 
 use Da\Google\Places\Client\PlaceClient;
